c51f84a300ea8fdf09ae7fd0818a647f9582bac5
verywelldone <ionutbogdan11@gmail.com>
2022-09-23 12:56:47 +0300

    initial commit

---
+++ src/main/java/uptrip/UptripApplication.java
@@ -0,0 +1,13 @@
+package uptrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UptripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uptrip/UptripApplicationTests.java
@@ -0,0 +1,13 @@
+package uptrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
