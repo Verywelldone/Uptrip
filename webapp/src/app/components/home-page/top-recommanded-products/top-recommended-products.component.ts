2c1842c0197b2b1935d35b831b68438a789ed9b8
VeryWellDone <ionutbogdan11@gmail.com>
2022-10-23 23:19:02 +0300

    Chore: random stuff

--- webapp/src/app/components/home-page/top-recommanded-products/top-recommended-products.component.ts
+++ webapp/src/app/components/home-page/top-recommanded-products/top-recommended-products.component.ts
@@ -8,23 +8,23 @@ import {MessageService} from "primeng/api";
   styleUrls: ['./top-recommended-products.component.css']
 })
 export class TopRecommendedProducts implements OnInit {
-  responsiveOptions = [
-    {
-      breakpoint: '1024px',
-      numVisible: 3,
-      numScroll: 3
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 2,
-      numScroll: 2
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1,
-      numScroll: 1
-    }
-  ];
+  // responsiveOptions = [
+  //   {
+  //     breakpoint: '1024px',
+  //     numVisible: 3,
+  //     numScroll: 3
+  //   },
+  //   {
+  //     breakpoint: '768px',
+  //     numVisible: 2,
+  //     numScroll: 2
+  //   },
+  //   {
+  //     breakpoint: '560px',
+  //     numVisible: 1,
+  //     numScroll: 1
+  //   }
+  // ];
 
   products: Array<ProductItem> = [];
 
